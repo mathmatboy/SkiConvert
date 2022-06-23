44d3d3b31556ab61bf6c4e3bb201300a73dd9cea
mathieu <math1242@hotmail.com>
2022-06-23 06:27:13 -0400

    V1.1
    V1.1

--- App.py
+++ App.py
@@ -7,12 +7,10 @@ from numpy import insert
 from pyparsing import withClass
 import Point_Fonction
 
-#------Variable------#
-
-#--Fonction permettant d'ouvrir un fichier--#
+"""Fonction permettant d'ouvrir un fichier"""
 def showFrame():
     configuration_frame.place(x=180 , y=300)
-
+"""Fonction pour enregistrer nos fichier en CSV """
 def save_pts_to_csv_file():
 
     filetypes = (
@@ -51,8 +49,8 @@ def select_files():
     file_entry.insert(0, filenames)   
 
 window = Tk()
-window.iconbitmap('C:\\Users\\mathieu\\Documents\\project\\Utopie MFG\\SkiConverter\\logo.ico')
-window.title("SkiConverter - Utopie tunnel")
+window.iconbitmap('C:\\Users\\mathieu\\Documents\\project\\NAME\\SkiConverter\\logo.ico')
+window.title("SkiConverter - ****** tunnel")
 window.geometry("920x600")
 window.resizable(False, False)
 window.config(background=('grey'))
@@ -65,15 +63,15 @@ configuration_frame= Frame(window, bg=("white"))
 label_Title = Label(navigation_menu, text="SkiConverter", font=("Courrier", 40), bg=("#2E74DA"), fg=("white"))
 label_Title.grid(row=0, column=0)
 
-img_bp_config = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\Utopie MFG\\SkiConverter\\bp_config.png")
+img_bp_config = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\NAME\\SkiConverter\\bp_config.png")
 config_button = Button(navigation_menu, image=img_bp_config, bg=("#2E74DA"), bd=0, activebackground=("#2E74DA"), command=showFrame)
 config_button.grid(row=0, column=1)
 
-img_bp_upload = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\Utopie MFG\\SkiConverter\\bp_upload.png")
+img_bp_upload = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\NAME\\SkiConverter\\bp_upload.png")
 upload_Button = Button(navigation_menu, image=img_bp_upload, bg=("#2E74DA"), bd=0, activebackground=("#2E74DA"), command=select_files)
 upload_Button.grid(row=0, column=2)
 
-img_bp_quit = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\Utopie MFG\\SkiConverter\\bp_quit.png")
+img_bp_quit = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\NAME\\SkiConverter\\bp_quit.png")
 quit_button = Button(navigation_menu, image=img_bp_quit, bg=("#2E74DA"), bd=0, activebackground=("#2E74DA"), command=window.destroy)
 quit_button.grid(row=0, column=3)
 
@@ -84,11 +82,11 @@ str_var = StringVar(window)
 file_entry = Entry(window, textvariable=str_var, bg=("#D3D7DC"), width=90)
 file_entry.place(x=180, y=150)
 
-img_bp_CSV = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\Utopie MFG\\SkiConverter\\bp_CSV.png")
+img_bp_CSV = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\NAME\\SkiConverter\\bp_CSV.png")
 CSV_button = Button(window, image=img_bp_CSV, bg=("grey"), bd=0, activebackground=("grey"), command=save_pts_to_csv_file)
 CSV_button.place(x=180, y=200)
 
-img_bp_view = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\Utopie MFG\\SkiConverter\\bp_view.png")
+img_bp_view = PhotoImage(file="C:\\Users\\mathieu\\Documents\\project\\NAME\\SkiConverter\\bp_view.png")
 view_button = Button(window, image=img_bp_view, bg=("grey"), bd=0, activebackground=("grey"), command=pts_mathplot)
 view_button.place(x=520, y=200)
 
